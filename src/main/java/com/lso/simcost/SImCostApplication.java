55212eb51e8923b5a235e6286ca8f6a4de0f44ae
LeoSuarezO <leonardo901.ls2@gmail.com>
2023-07-24 18:23:42 -0500

    First commit

---
+++ src/main/java/com/lso/simcost/SImCostApplication.java
@@ -0,0 +1,13 @@
+package com.lso.simcost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SImCostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SImCostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lso/simcost/SImCostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lso.simcost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SImCostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
